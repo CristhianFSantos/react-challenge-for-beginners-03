be5ad2bf8267bebb5fb2b1acdc25211cee8b1ca0
Cristhian Santos <cristhianfelipe0@gmail.com>
2023-01-26 21:59:58 -0300

    ✨ feat: create card form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useCallback } from "react";
 import "./App.scss";
+import { CustomButtom } from "./components/CustomButton/CustomButton";
+import { Header } from "./components/Header/Header";
 
 function App() {
-  return <div className="App"></div>;
+  const handleSubmit = useCallback(() => {
+    console.log("Formulário enviado");
+  }, []);
+
+  return (
+    <div className="App">
+      <Header />
+      <div className="card">
+        <CustomButtom label="Enviar Formulário" onClick={handleSubmit} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
