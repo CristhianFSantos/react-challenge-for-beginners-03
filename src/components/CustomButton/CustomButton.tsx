8090697be767fa1336bb0bb5484546cbb59eece6
Cristhian Santos <cristhianfelipe0@gmail.com>
2023-01-28 16:39:18 -0300

    ✨ feat: add disabled feature in custom button

--- src/components/CustomButton/CustomButton.tsx
+++ src/components/CustomButton/CustomButton.tsx
@@ -3,15 +3,19 @@ import "./styles.scss";
 
 export interface CustomButtomProps {
   label: string;
+  disabled?: boolean;
   onClick: () => void;
 }
 
 const CustomButtom: React.FC<CustomButtomProps> = (
   props: CustomButtomProps
 ) => {
+  const { label, disabled, onClick } = props;
+  const className = disabled ? "custom-button disabled" : "custom-button";
+
   return (
-    <button className="custom-button" onClick={props.onClick}>
-      {props.label}
+    <button disabled={disabled} className={className} onClick={onClick}>
+      {label}
     </button>
   );
 };
